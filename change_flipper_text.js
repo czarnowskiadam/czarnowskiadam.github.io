a1d787b4363ff2ab6e33a6c641407faaa77e5760
czarnowskiadam <73715546+czarnowskiadam@users.noreply.github.com>
2024-08-15 18:54:44 +0200

    Add files via upload

--- change_flipper_text.js
+++ change_flipper_text.js
@@ -1,9 +1,21 @@
 function updateText() {
     const flipContainers = document.querySelectorAll('.flip-container .front p');
+    const currentFile = window.location.pathname;
+    const fileName = currentFile.substring(currentFile.lastIndexOf('/') + 1);
     if (window.innerWidth < 1000) {
-        flipContainers.forEach(p => p.textContent = 'Click me!');
-    } else {
-        flipContainers.forEach(p => p.textContent = 'Hover over me!');
+        if (fileName == 'index.html') {
+            flipContainers.forEach(p => p.textContent = 'Click me!');
+        }
+        if (fileName == 'polish.html') {
+            flipContainers.forEach(p => p.textContent = 'Kliknij na mnie!');
+        }        
+    } else {        
+        if (fileName == 'index.html') {
+            flipContainers.forEach(p => p.textContent = 'Hover over me!');
+        }
+        if (fileName == 'polish.html') {
+            flipContainers.forEach(p => p.textContent = 'Najedź na mnie!');
+        }
     }
 }
 
---
+++ switch_view.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const homeButton = document.querySelector('#homeButton');
+    const resumeButton = document.querySelector('#resumeButton');
+    const projectsButton = document.querySelector('#projectsButton');
+    const contactButton = document.querySelector('#contactButton');
+
+    const languageButton = document.querySelector('#languageButton');
+
+    const switchHome = document.querySelector('.switch-home');
+    const switchResume = document.querySelector('.switch-resume');
+    const switchProjects = document.querySelector('.switch-projects');
+    const switchContact = document.querySelector('.switch-contact');
+
+
+    function getViewList() {
+        return [
+            switchHome.style.visibility,
+            switchResume.style.visibility,
+            switchProjects.style.visibility,
+            switchContact.style.visibility
+        ];
+    }
+
+    function switchView(viewToShow) {        
+        switchHome.style.visibility = 'hidden';
+        switchResume.style.visibility = 'hidden';
+        switchProjects.style.visibility = 'hidden';
+        switchContact.style.visibility = 'hidden';
+        
+        viewToShow.style.visibility = 'visible';
+    }
+    
+    homeButton.addEventListener('click', function() {        
+        switchView(switchHome);     
+        console.log(getViewList());
+    });
+
+    resumeButton.addEventListener('click', function() {        
+        switchView(switchResume);   
+        console.log(getViewList()); 
+    });
+
+    projectsButton.addEventListener('click', function() {
+        switchView(switchProjects); 
+        console.log(getViewList()); 
+    });
+
+    contactButton.addEventListener('click', function() {
+        switchView(switchContact);  
+        console.log(getViewList()); 
+    });
+
+    languageButton.addEventListener('click', function() {
+        const currentFile = window.location.pathname;
+        const fileName = currentFile.substring(currentFile.lastIndexOf('/') + 1);
+        
+        if (fileName == 'index.html') {
+            window.location.href = 'polish.html';
+        }
+        if (fileName == 'polish.html') {
+            window.location.href = 'index.html';
+        }
+    });
+});
